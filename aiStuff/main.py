738874eaef91c8b29357d99dd5e4e117302b79e8
ralsei <131580914+KoteykaMeaw@users.noreply.github.com>
2024-06-21 10:33:41 +0700

    Update main.py

--- aiStuff/main.py
+++ aiStuff/main.py
@@ -3,6 +3,8 @@ from settings import TOKEN
 from discord.ext import commands
 from discord import app_commands
 import lgoic
+import time
+import os
 
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
@@ -17,11 +19,25 @@ async def on_ready():
     except Exception as e:
         print(e)
 
+last_message_times = {}
+
+COOLDOWN = 5
 
 @bot.event
 async def on_message(message):
     if message.author != bot.user:
-        await message.author.send("Generating image, please wait.")
+        user_id = message.author.id
+        if user_id in last_message_times:
+            elapsed_time = time.time() - last_message_times[user_id]
+            if elapsed_time < COOLDOWN:
+                await message.author.send(
+                    f"Пожалуйста, подождите {COOLDOWN - elapsed_time:.1f} секунд перед отправкой следующего запроса.")
+                return
+
+            # Обновление времени последнего сообщения
+        last_message_times[user_id] = time.time()
+
+        generatingtext = await message.author.send("Генерирую картинку..")
 
         api = lgoic.Text2ImageAPI('https://api-key.fusionbrain.ai/', 'key',
                             'secret key')
@@ -30,6 +46,19 @@ async def on_message(message):
         image = api.generate_and_save_image(message, model_id, imagename)
 
         await message.author.send(file=discord.File(imagename))
+
+        os.remove(imagename)
+        await generatingtext.delete()
+
+@bot.tree.command(name='help', description='What this bot does?')
+async def help(interaction: discord.Integration):
+    await interaction.response.send_message("Привет! Я бот для генерации картинок. Отправь мне текстовое описание желаемой картинки, и я создам ее. Например, напиши: 'Котик с крыльями ангела, сидящий на облаке' " )
+
+
+
+
+
+
 
 
 bot.run(token=TOKEN)
